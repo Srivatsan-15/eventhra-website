3dec0e220a0ed179c495492abae0a4862ae23bda
SrivatsanIITM <23f2000296@ds.study.iitm.ac.in>
2025-06-17 02:11:18 +0530

    UI Addition in InterEvents and IntaEvents

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,6 +1,8 @@
 import "../styles/Events.scss";
 import NavBar from "../components/NavBar";
 import React from "react";
+import IntraEvents from "../components/IntraEvents";
+import InterEvents from "../components/InterEvents";
 
 type Props = {
   navVariable: number;
@@ -15,7 +17,7 @@ function Events({ navVariable, setnavVariable}: Props) {
     }
   return (
     <div className="events">
-      <div className="eventsbg vh-97 w-100">
+      <div className="eventsbg w-100">
         <NavBar navVariable={navVariable} setnavVariable={setnavVariable} />
         <div className="row m-5">
             <div className="col d-flex justify-content-start align-items-center gap-5 ms-5">
@@ -23,7 +25,9 @@ function Events({ navVariable, setnavVariable}: Props) {
                 <h4 className={isIntra ? "notSelected" : "selected"} onClick={() => setisIntra(false)}>Inter College Events</h4>
             </div>
         </div>
-        
+        <div className="m-3">
+        {isIntra ? <IntraEvents /> : <InterEvents />}
+        </div>
       </div>
     </div>
   );
